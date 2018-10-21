755f14523c1e4364c9a22771fc91d3c893f7caf6
Bo Fu <fubo130@gmail.com>
2018-10-21 17:43:43 -0500

    fit life app project file

---
+++ FitLifeApp/FitLifeApp/FitLifeApp-proj/src/main/java/edu/baylor/ecs/FitLifeApp/Main.java
@@ -0,0 +1,42 @@
+package edu.baylor.ecs.FitLifeApp;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Properties;
+
+import javax.swing.*;
+import javax.swing.table.TableModel;
+
+import org.jdatepicker.impl.JDatePanelImpl;
+import org.jdatepicker.impl.JDatePickerImpl;
+import org.jdatepicker.impl.UtilDateModel;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.CreationHelper;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+
+public class Main {
+	public static void main(String[] args) {
+		WindowManager wm = new WindowManager();
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				wm.toLogIn();
+			}
+		});
+	}
+}
+		
