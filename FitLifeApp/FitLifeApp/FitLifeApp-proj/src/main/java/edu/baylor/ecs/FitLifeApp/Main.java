08531ffa8e88bda30a783949ea79706e5ee22069
Yufan Xu <XUYU@ECS.BAYLOR.EDU>
2018-10-22 18:44:53 -0500

    more GUI for Home screen

--- FitLifeApp/FitLifeApp/FitLifeApp-proj/src/main/java/edu/baylor/ecs/FitLifeApp/Main.java
+++ FitLifeApp/FitLifeApp/FitLifeApp-proj/src/main/java/edu/baylor/ecs/FitLifeApp/Main.java
@@ -1,42 +1,29 @@
-package edu.baylor.ecs.FitLifeApp;
+/*
+ * Yufan Xu
+ * labSwing
+ */
 
-import java.awt.*;
+
+package swing;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Properties;
+import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import javax.swing.*;
-import javax.swing.table.TableModel;
-
-import org.jdatepicker.impl.JDatePanelImpl;
-import org.jdatepicker.impl.JDatePickerImpl;
-import org.jdatepicker.impl.UtilDateModel;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellStyle;
-import org.apache.poi.ss.usermodel.CreationHelper;
-import org.apache.poi.ss.usermodel.Font;
-import org.apache.poi.ss.usermodel.IndexedColors;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-
 
 public class Main {
-	public static void main(String[] args) {
-		WindowManager wm = new WindowManager();
+	public static void main(String[]args)  {
+
+		final WindowManager m1 = new WindowManager();
+		
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				wm.toLogIn();
+			public void run () {
+				m1.toLogIn();
 			}
 		});
 	}
 }
-		
